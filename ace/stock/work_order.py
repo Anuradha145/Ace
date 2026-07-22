07e28a3d199776b6c55b5e6d823593f37b529f14
Anuradha145 <anuradha@fosserp.com>
2026-07-22 17:16:11 +0530

    chore: remove unnecessary work order transfer sync patches

--- ace/stock/work_order.py
+++ ace/stock/work_order.py
@@ -13,8 +13,6 @@ def update_material_transferred_for_work_order(work_order):
 	if not work_order:
 		return
 
-	recalculate_required_item_transferred_qty(work_order)
-
 	wo = frappe.db.get_value(
 		"Work Order",
 		work_order,
@@ -56,37 +54,3 @@ def update_material_transferred_for_work_order(work_order):
 		material_transferred_for_manufacturing,
 		update_modified=False,
 	)
-	sync_job_card_transferred_qty(work_order, material_transferred_for_manufacturing)
-
-
-def recalculate_required_item_transferred_qty(work_order):
-	wo = frappe.get_doc("Work Order", work_order)
-	wo.update_transferred_qty_for_required_items()
-
-
-def sync_job_card_transferred_qty(work_order, transferred_qty):
-	if frappe.db.get_value("Work Order", work_order, "transfer_material_against") != "Work Order":
-		return
-
-	job_cards = frappe.get_all(
-		"Job Card",
-		filters={
-			"work_order": work_order,
-			"docstatus": ("<", 2),
-			"skip_material_transfer": 0,
-		},
-		fields=["name", "for_quantity", "process_loss_qty"],
-	)
-
-	for job_card in job_cards:
-		job_card_transferred_qty = min(
-			flt(job_card.for_quantity) + flt(job_card.process_loss_qty),
-			flt(transferred_qty),
-		)
-		frappe.db.set_value(
-			"Job Card",
-			job_card.name,
-			"transferred_qty",
-			job_card_transferred_qty,
-			update_modified=False,
-		)
