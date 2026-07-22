36408d86a88065ff85a4c5a3cf1db207fb48d9e6
Anuradha145 <anuradha@fosserp.com>
2026-07-22 14:25:54 +0530

    fix: make job card inventory dimensions optional

---
+++ ace/patches/v16_0/make_job_card_inventory_dimensions_optional.py
@@ -0,0 +1,26 @@
+import frappe
+
+
+JOB_CARD_DIMENSION_FIELDS = (
+	"project_aa",
+	"bin_location",
+)
+
+
+def execute():
+	for fieldname in JOB_CARD_DIMENSION_FIELDS:
+		field = frappe.db.exists("Custom Field", {"dt": "Job Card", "fieldname": fieldname})
+		if not field:
+			continue
+
+		frappe.db.set_value(
+			"Custom Field",
+			field,
+			{
+				"reqd": 0,
+				"mandatory_depends_on": "",
+			},
+			update_modified=False,
+		)
+
+	frappe.clear_cache(doctype="Job Card")
